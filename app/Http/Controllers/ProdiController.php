4d0f1c9ed812b2caa6a34aaffbd5122d23a5fdbb
SirJayyy123 <rubbyafkar1121@gmail.com>
2024-10-28 14:09:01 +0700

    +halaman program studi

---
+++ app/Http/Controllers/ProdiController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Prodi;
+
+class ProdiController extends Controller
+{
+    public function index()
+{
+    return view('prodi.index');
+}
+}
