1f4cf6a22fbd5b8058c67dac78ddb97b2c38c83c
SirJayyy123 <rubbyafkar1121@gmail.com>
2024-10-29 23:40:55 +0700

    UTS BERES BOS

--- app/Http/Controllers/ProdiController.php
+++ app/Http/Controllers/ProdiController.php
@@ -7,8 +7,55 @@ use App\Models\Prodi;
 
 class ProdiController extends Controller
 {
-    public function index()
+    
+public function index()
 {
-    return view('prodi.index');
+    $prodis = Prodi::orderBy('id', 'desc')->get();
+    return view('prodi.index', compact('prodis'));
+}
+
+public function create()
+{
+    return view('prodi.create');
+}
+
+public function save(Request $request)
+{
+    $request->validate([
+        'nama' => 'required'
+    ]);
+
+    Prodi::create([
+        'nama' => $request->nama
+    ]);
+    return redirect()->route('/prodi')->with('success', 'Program Studi berhasil ditambahkan');
+}
+
+public function edit ($id)
+{
+    $prodi = Prodi::findOrFail($id);
+    return view('prodi.edit', compact('/prodi'));
+}
+
+public function update(Request $request, $id)
+{
+    $request->validate([
+        'nama' => 'required'
+    ]);
+
+    $prodi = Prodi::findOrFail($id);
+    $prodi->update([
+        'nama' => $request->nama
+    ]);
+    return redirect()->route('/prodi')->with('success', 'Program Studi berhasil diupdated');
+}
+
+
+public function delete ($id)
+{
+    $prodi = Prodi::findOrFail($id);
+    $prodi->delete();
+
+    return redirect()->route('/prodi')->with('success', 'Data Program Studi berhasil dihapus');
 }
 }
